cb6020deba940085bdd7df09391ae57b2ec36ae0
Sanskruti-Rathi <sanskrutirathi1644@gmail.com>
2026-02-14 14:38:59 +0530

    uploading video completed

--- lib/views/widgets/screens/add_video_screen.dart
+++ lib/views/widgets/screens/add_video_screen.dart
@@ -1,90 +1,88 @@
-import 'dart:io';
-
+import 'dart:typed_data';
 import 'package:flutter/material.dart';
-//import 'package:get/get_connect/http/src/utils/utils.dart';
+import 'package:flutter/foundation.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:reel_stream/constants.dart';
 import 'package:reel_stream/views/widgets/screens/confirm_screen.dart';
+import 'dart:io';
 
 class AddVideoScreen extends StatelessWidget {
   const AddVideoScreen({super.key});
 
-  pickVideo (ImageSource src,BuildContext context)async{
-    final video=await ImagePicker().pickVideo(source: src);
-    if (video != null){
-      Navigator.of(context).push
-      (MaterialPageRoute(
-        builder: (context)=> ConfirmScreen(
-          videoFile: File(video.path),
-          videoPath: video.path,
-          
-        ),
-      ),
-      );
+  pickVideo(ImageSource src, BuildContext context) async {
+    final video = await ImagePicker().pickVideo(source: src);
+
+    if (video != null) {
+      if (kIsWeb) {
+        // 🌐 WEB
+        Uint8List bytes = await video.readAsBytes();
+
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => ConfirmScreen(
+              videoBytes: bytes,
+              videoPath: video.path,
+            ),
+          ),
+        );
+      } else {
+        // 📱 MOBILE
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => ConfirmScreen(
+              videoFile: File(video.path),
+              videoPath: video.path,
+            ),
+          ),
+        );
+      }
     }
   }
 
-  showOptionsDialog(BuildContext context){
-    return showDialog(context:context,builder:(context) => SimpleDialog(
-      children: [
-        SimpleDialogOption(
-        onPressed:() => pickVideo(ImageSource.gallery, context),
-        child: Row(
-          children: const [
-            Icon(Icons.image),
-            Padding(
-              padding: EdgeInsets.all(7.0),
-              child: Text(
-                "Gallery",
-                style: TextStyle(
-                  fontSize: 20
-                  ),
-                  ),
+  showOptionsDialog(BuildContext context) {
+    return showDialog(
+      context: context,
+      builder: (context) => SimpleDialog(
+        children: [
+          SimpleDialogOption(
+            onPressed: () => pickVideo(ImageSource.gallery, context),
+            child: const Row(
+              children: [
+                Icon(Icons.image),
+                Padding(
+                  padding: EdgeInsets.all(7.0),
+                  child: Text("Gallery", style: TextStyle(fontSize: 20)),
+                ),
+              ],
             ),
-          ],
-        ),
-
-        ),
-        SimpleDialogOption(
-        onPressed:() => pickVideo(ImageSource.camera, context),
-        child: Row(
-          children: const [
-            Icon(Icons.camera_alt),
-            Padding(
-              padding: EdgeInsets.all(7.0),
-              child: Text(
-                "Camera",
-                style: TextStyle(
-                  fontSize: 20
-                  ),
-                  ),
+          ),
+          SimpleDialogOption(
+            onPressed: () => pickVideo(ImageSource.camera, context),
+            child: const Row(
+              children: [
+                Icon(Icons.camera_alt),
+                Padding(
+                  padding: EdgeInsets.all(7.0),
+                  child: Text("Camera", style: TextStyle(fontSize: 20)),
+                ),
+              ],
             ),
-          ],
-        ),
-
-        ),
-        SimpleDialogOption(
-        onPressed:() => Navigator.of(context).pop(),
-        child: Row(
-          children: const [
-            Icon(Icons.cancel),
-            Padding(
-              padding: EdgeInsets.all(7.0),
-              child: Text(
-                "Cancel",
-                style: TextStyle(
-                  fontSize: 20
-                  ),
-                  ),
+          ),
+          SimpleDialogOption(
+            onPressed: () => Navigator.of(context).pop(),
+            child: const Row(
+              children: [
+                Icon(Icons.cancel),
+                Padding(
+                  padding: EdgeInsets.all(7.0),
+                  child: Text("Cancel", style: TextStyle(fontSize: 20)),
+                ),
+              ],
             ),
-          ],
-        ),
-
-        ),
-      ],
-    ));
-
-
+          ),
+        ],
+      ),
+    );
   }
 
   @override
@@ -97,20 +95,17 @@ class AddVideoScreen extends StatelessWidget {
             width: 190,
             height: 50,
             decoration: BoxDecoration(color: buttonColor),
-            child: const Center(child: Text(
-              "Add Video",
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.black,
-                fontWeight: FontWeight.bold
-                ),
-                ),
-
+            child: const Center(
+              child: Text(
+                "Add Video",
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold),
+              ),
+            ),
           ),
-          
         ),
-          ),
-          
       ),
     );
   }
--- lib/views/widgets/screens/confirm_screen.dart
+++ lib/views/widgets/screens/confirm_screen.dart
@@ -1,32 +1,59 @@
-import 'package:flutter/material.dart';
 import 'dart:io';
+import 'dart:typed_data';
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
 import 'package:reel_stream/views/widgets/text_input_field.dart';
 import 'package:video_player/video_player.dart';
+import 'package:get/get.dart';
+
 class ConfirmScreen extends StatefulWidget {
-  final File  videoFile;
+  final File? videoFile;
+  final Uint8List? videoBytes;
   final String videoPath;
 
-  const ConfirmScreen({super.key,required this.videoFile, required this.videoPath});
+  const ConfirmScreen({
+    super.key,
+    this.videoFile,
+    this.videoBytes,
+    required this.videoPath,
+  });
 
   @override
-
   State<ConfirmScreen> createState() => _ConfirmScreenState();
 }
 
 class _ConfirmScreenState extends State<ConfirmScreen> {
   late VideoPlayerController controller;
-  TextEditingController songController = TextEditingController();
-  TextEditingController captionController = TextEditingController();
+
+  TextEditingController _songController = TextEditingController();
+  TextEditingController _captionController = TextEditingController();
+
+  UploadVideoController uploadVideoController = Get.put(UploadVideoController());
+
   @override
   void initState() {
     super.initState();
-    setState(() {
-      controller = VideoPlayerController.file(widget.videoFile);
+
+    if (kIsWeb) {
+      controller = VideoPlayerController.networkUrl(
+        Uri.parse(widget.videoPath),
+      );
+    } else {
+      controller = VideoPlayerController.file(widget.videoFile!);
+    }
+
+    controller.initialize().then((_) {
+      setState(() {});
+      controller.play();
+      controller.setLooping(true);
+      controller.setVolume(1);
     });
-    controller.initialize();
-    controller.play();
-    controller.setVolume(1);
-    controller.setLooping(true);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 
   @override
@@ -35,62 +62,56 @@ class _ConfirmScreenState extends State<ConfirmScreen> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            const SizedBox(
-              height: 30
-              ),
-              SizedBox(
-                height: MediaQuery.of(context).size.height/1.5,
-                width:MediaQuery.of(context).size.width,
-                child: VideoPlayer(controller),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              SingleChildScrollView(
-                scrollDirection: Axis.vertical,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 10),
-                  width:MediaQuery.of(  context).size.width-20,
-                  child: TextInputField(
-                    controller: songController, 
-                    labelText: "Song Name", 
-                    icon: Icons.music_note,),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                Container(
-                  margin: const EdgeInsets.symmetric(horizontal: 10),
-                  width:MediaQuery.of(  context).size.width-20,
-                  child: TextInputField(
-                    controller: captionController, 
-                    labelText: "Caption", 
-                    icon: Icons.closed_caption,),
-                ),
-                const SizedBox(
-                  height: 10,
-                ),
-                ElevatedButton(onPressed:(){}, 
-                child: Text("Share",style : TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                ),
-                ),
-                ),
+            const SizedBox(height: 30),
 
-              ],
-                ),
-              )
-        ],
-        )
-          
+            SizedBox(
+              height: MediaQuery.of(context).size.height / 1.5,
+              width: MediaQuery.of(context).size.width,
+              child: controller.value.isInitialized
+                  ? VideoPlayer(controller)
+                  : const Center(child: CircularProgressIndicator()),
+            ),
+
+            const SizedBox(height: 30),
+
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              width: MediaQuery.of(context).size.width - 20,
+              child: TextInputField(
+                controller: _songController,
+                labelText: "Song Name",
+                icon: Icons.music_note,
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            Container(
+              margin: const EdgeInsets.symmetric(horizontal: 10),
+              width: MediaQuery.of(context).size.width - 20,
+              child: TextInputField(
+                controller: _captionController,
+                labelText: "Caption",
+                icon: Icons.closed_caption,
+              ),
+            ),
+
+            const SizedBox(height: 10),
+
+            ElevatedButton(
+              onPressed: () => uploadVideoController.uploadVideo(
+                _songController.text,
+                _captionController.text,
+                widget.videoPath,
+              ),
+              child: const Text(
+                "Share",
+                style: TextStyle(fontSize: 20, color: Colors.white),
+              ),
+            ),
+          ],
+        ),
       ),
-
-
-
     );
   }
 }
